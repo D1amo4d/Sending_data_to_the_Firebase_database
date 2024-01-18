b428af19a934f5dd8ab1d4ca227f517893f38df4
almaz <n58773014@gmail.com>
2024-01-18 14:06:55 +0600

    Added data acquisition

--- app/src/main/java/com/example/youtube_test/MainActivity.kt
+++ app/src/main/java/com/example/youtube_test/MainActivity.kt
@@ -5,12 +5,13 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnSend: MaterialButton
     private lateinit var etName: EditText
+    private lateinit var references: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,26 +19,46 @@ class MainActivity : AppCompatActivity() {
 
         btnSend = findViewById(R.id.btn_send)
         etName = findViewById(R.id.et_name)
-
-        val database = FirebaseDatabase.getInstance()
-        val references = database.getReference("users")
+        references = FirebaseDatabase.getInstance().getReference("users")
 
         btnSend.setOnClickListener {
-            val userNameText = etName.text.toString()
-            val userId = references.push().key
-            val userReference = references.child(userId.toString())
+            sendMessageToDatabase(etName.text.toString())
+        }
 
-            userReference.setValue(userNameText).addOnCompleteListener { task ->
+        val userIdToRetrieve = "здесь_ваш_идентификатор_пользователя"
+        retrieveDataFromDatabase(userIdToRetrieve)
+    }
+
+    private fun sendMessageToDatabase(message: String) {
+        val userId = references.push().key
+        references.child(userId.toString()).setValue(message)
+            .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     showToast("Сообщение отправлено")
                 } else {
                     showToast("Ошибка при отправке сообщения")
                 }
             }
-        }
+    }
+
+    private fun retrieveDataFromDatabase(userId: String) {
+        references.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val userName = dataSnapshot.getValue(String::class.java)
+                userName?.let {
+                    showToast("Имя пользователя: $it")
+                    etName.setText(it)
+                } ?: showToast("Пользователь не найден")
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                showToast("Ошибка при чтении данных из базы данных: ${databaseError.message}")
+            }
+        })
     }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
+
